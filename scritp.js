a06bd4088ef9fd4590743294ba75f34c62837bbf
corinneremy <corinne7086@hotmail.fr>
2023-08-28 11:25:43 +0200

    huuu

---
+++ scritp.js
@@ -0,0 +1,12 @@
+document.getElementById("button").onclick = function(){
+     document.getElementById("info").classList.toggle("hidden");
+    
+    document.getElementById("open").classList.toggle("hidden");
+    document.getElementById("fleche").classList.toggle("hidden")
+};
+
+document.getElementById("heart").onclick = function(){
+
+   document.getElementById("heartOpen").classList.toggle("hidden");
+   document.getElementById("heartClose").classList.toggle("hidden")
+};
