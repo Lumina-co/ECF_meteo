fab1fb550bdfd22c78edd982760f1ec79005c5dd
corinneremy <corinne7086@hotmail.fr>
2023-08-31 14:08:26 +0200

    intégration maquette_météo

--- scritp.js
+++ scritp.js
@@ -1,8 +1,23 @@
-document.getElementById("button").onclick = function(){
+document.getElementById("open").onclick = function(){ 
+// lorsque l'élément id est cliqué  une fonction est éxécutée
+
      document.getElementById("info").classList.toggle("hidden");
+     document.getElementById("info").classList.toggle("");
+// cette fonction utilise la méthode classlist.toogle pour ajouter ou supprimer la class hidden
+
+      // document.getElementById("open").classList.toggle("hidden");
+      // ensuite la même method est utilisée pour ajouté
+ 
+};
+
+document.getElementById("fleche").onclick = function(){
+
+   document.getElementById("info").classList.toggle("hidden");
+   document.getElementById("info").classList.toggle("");
     
-    document.getElementById("open").classList.toggle("hidden");
-    document.getElementById("fleche").classList.toggle("hidden")
+
+//   document.getElementById("open").classList.toggle("hidden");
+
 };
 
 document.getElementById("heart").onclick = function(){
@@ -10,3 +25,4 @@ document.getElementById("heart").onclick = function(){
    document.getElementById("heartOpen").classList.toggle("hidden");
    document.getElementById("heartClose").classList.toggle("hidden")
 };
+
